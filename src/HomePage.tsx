c78c68dfd54b8225ac2766cc9887b8410d0e2576
Nick G <nicholasgalante1997@gmail.com>
2022-02-27 18:11:24 -0700

    adjusted text

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './styles/home.module.css';
 import { Subheading, Page, SectionBreak, Heading, BeginButton } from './components';
 
-const subheading = `This is a collection of stories about a weird new reality. If it's post modern, it's unfortunately by accident. If it any part you find yourself, "this is a stroke of art...", I would also advocate that that was probably an accident.`;
+const subheading = `This is a collection of stories about a weird new reality. If it's post modern, it's unfortunately by accident. If it any part you find yourself thinking, "this is a stroke of art...", I would also advocate that that was probably an accident.`;
 interface HomePageProps {
   onClick: () => void;
 }
